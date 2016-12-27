76743283a0dab8a9eaefc58527058afe5bec31e4
popomore <sakura9515@gmail.com>
2016-12-27 18:46:56 +0800

    deps upgrade debug

---
+++ .autod.conf.js
@@ -0,0 +1,19 @@
+'use strict';
+
+module.exports = {
+  write: true,
+  prefix: '^',
+  devprefix: '^',
+  exclude: [
+    'test/fixtures',
+  ],
+  devdep: [
+    'autod',
+    'egg-bin',
+    'egg-ci',
+  ],
+  keep: [
+  ],
+  semver: [
+  ],
+};
